b5a98576331cc47ca7e386a88d46a63282d04f7b
[yangyang] <[yangyangli@uma.es]>
2019-04-29 16:37:14 +0200

    fibonacciRabbits

--- src/main/java/progA/Fibonacci.java
+++ src/main/java/progA/Fibonacci.java
@@ -2,20 +2,19 @@ package progA;
 
 /**
  * Hello world!
- *
  */
 public class Fibonacci {
 
-    int r;
+  double result;
 
-    public int compute(int n) {
-        if (n == 1 || n == 2) {
-            r = 1;
-        } else if(n<=0){
-            throw new RuntimeException("can not be negative");
-        }
-            else
-            r = compute(n - 1) + compute(n - 2);
-        return r;
+  public double compute(int n) {
+    if (n == 1 || n == 2) {
+      result = 1;
+    } else if (n <= 0) {
+      throw new RuntimeException("can not be negative");
+    } else {
+      result = compute(n - 1) + compute(n - 2);
     }
+    return result;
+  }
 }
