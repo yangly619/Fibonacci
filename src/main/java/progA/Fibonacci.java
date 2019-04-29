0ff04cf293699e36cacb70d7acd131153f0bcabd
[yangyang] <[yangyangli@uma.es]>
2019-04-29 12:53:09 +0200

    second commit

--- src/main/java/progA/Fibonacci.java
+++ src/main/java/progA/Fibonacci.java
@@ -11,7 +11,10 @@ public class Fibonacci {
     public int compute(int n) {
         if (n == 1 || n == 2) {
             r = 1;
-        } else
+        } else if(n<=0){
+            throw new RuntimeException("can not be negative");
+        }
+            else
             r = compute(n - 1) + compute(n - 2);
         return r;
     }
--- src/test/java/progA/FibonacciTest.java
+++ src/test/java/progA/FibonacciTest.java
@@ -1,4 +1,5 @@
 package progA;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 /**
@@ -9,11 +10,33 @@ public class FibonacciTest
     /**
      * Rigorous Test :-)
      */
+    Fibonacci fibonacci;
+    @Before
+    public void setup() {
+        fibonacci=new Fibonacci();
+    }
     @Test
     public void should3Return2()
     {
-        Fibonacci fibonacci=new Fibonacci();
         int expected=2;
         assertEquals(expected,fibonacci.compute(3));
     }
+    @Test
+    public void should4Return3()
+    {
+        int expected=3;
+        assertEquals(expected,fibonacci.compute(4));
+    }
+    @Test
+    public void should9Return34()
+    {
+        int expected=34;
+        assertEquals(expected,fibonacci.compute(9));
+    }
+    @Test(expected=RuntimeException.class)
+    public void shouldNegativeThrowException()
+    {
+        fibonacci.compute(-1);
+    }
+
 }
